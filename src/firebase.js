0050b86bff080fc5c359768d6efae2fff893b9d7
EpicKiwiProCoder <bosnianmob@gmail.com>
2023-10-13 18:21:11 +0200

    Initialized firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBJieZTQDk2eD4FmuFkNxlDuyrsjwC7R-s",
+    authDomain: "chatsapp-8141c.firebaseapp.com",
+    projectId: "chatsapp-8141c",
+    storageBucket: "chatsapp-8141c.appspot.com",
+    messagingSenderId: "571055643127",
+    appId: "1:571055643127:web:71f96dc9152c9124b2b6f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
