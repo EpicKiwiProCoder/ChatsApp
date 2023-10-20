1bab6babbb8ed98d62450ae68ba3f50ca64feb36
EpicKiwiProCoder <bosnianmob@gmail.com>
2023-10-20 10:08:29 +0200

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import './App.css';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './firebase-config';
-import { SignIn, SignOut } from './components/Auth';
+
 import { ChatInput } from './components/ChatInput';
 import { UserCardPanel } from './components/UserCardPanel';
 import { ChatPanel } from './components/ChatPanel'
 import { Navbar } from './components/Navbar';
 
 function App() {
-  const [user] = useAuthState(auth);
-
   return (
     <div className="App">
       <Navbar />
       <div className="container">
         <div className="row">
-          <div className="col-4 border border-secondary rounded">
+          <div className="col-4 border-start border-end border-secondary border-2 pt-3">
             <div className="row">
               <UserCardPanel />
             </div>
 
           </div>
-          <div className="col-6 border border-primary rounded">
-
+          <div className="col-6 border-end border-secondary border-2 pt-3">
             <div className="row">
               <ChatPanel />
             </div>
             <div className="row p-5 fixed-bottom">
               <ChatInput />
             </div>
-
           </div>
           <div className="col-2"></div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,30 @@
-import logo from '../assets/logo.svg'; // Tell webpack this JS file uses this image
+import logo from '../assets/logo.png';
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand bg-dark">
+        <nav className="navbar navbar-dark bg-dark">
             <div className="container">
-                <a href="#" className="navbar-brand">
+                <a href="/" className="">
                     <img src={logo} alt="Chats App" />
                 </a>
+
+                <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#collapseTarget">
+                    <div className="navbar-toggler-icon"></div>
+                </button>
+
+                <div className="collapse navbar-collapse" id="collapseTarget">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a href="" className="nav-link text-bg-dark text-end fs-5">Berichten</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="" className="nav-link text-bg-dark text-end fs-5">Over deze website</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="" className="nav-link text-bg-dark text-end fs-5">Hoe werkt het?</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
