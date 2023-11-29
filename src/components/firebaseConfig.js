6c4d9737b109094121744eeee1752fd4ff2cb6b3
EpicKiwiProCoder <bosnianmob@gmail.com>
2023-11-29 19:58:47 +0100

    V15 Security Patch

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
+import apikey from "../secrets/apikey"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBJieZTQDk2eD4FmuFkNxlDuyrsjwC7R-s",
+    apiKey: apikey,
     authDomain: "chatsapp-8141c.firebaseapp.com",
     projectId: "chatsapp-8141c",
     storageBucket: "chatsapp-8141c.appspot.com",
